d7de69df6d9548c2240232756778d4134a78696b
IsaiDiaz <jsoza057@gmail.com>
2022-04-09 17:13:34 -0400

    Changing title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - 25 + 5 Clock</title>
+  <title>Pomodoro Clock Neon</title>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'><link rel="stylesheet" href="./style.css">
 
 </head>
